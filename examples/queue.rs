bdfc62f21daa8e60f7e48714bcb4559ea956efc8
Thomas Jespersen <laumann.thomas@gmail.com>
2014-10-20 16:04:02 +0200

    Add more code to queue.rs

--- examples/queue.rs
+++ examples/queue.rs
@@ -5,40 +5,69 @@
  *  - http://doc.rust-lang.org/src/collections/home/rustbuild/src/rust-buildbot/slave/nightly-linux/build/src/libcollections/vec.rs.html#105-109
  */
 
+extern crate alloc;
+extern crate core;
+extern crate quickcheck;
+
+use alloc::heap::{allocate};
+use core::mem;
+use core::ptr;
+
 // This could all reside in its own module 'queue.rs'
 pub struct Queue<T> {
     inp: uint,
     outp: uint,
-    buf: Vec<T> // change to *mut T
+    size: uint,
+    buf: *mut T
 }
 
 impl<T> Queue<T> {
     pub fn new(size: uint) -> Queue<T> {
-        Queue{inp:0, outp: 0, buf: Vec::with_capacity(size)}
-    }
-
-    pub fn size(&self) -> uint {
-        self.buf.capacity()
+        assert!(size > 0);
+        let sz = size.checked_mul(&mem::size_of::<T>())
+                     .expect("capacity overflow");
+        let buf = unsafe { allocate(sz, mem::min_align_of::<T>()) };
+        Queue{inp: 0, outp: 0, size: size, buf: buf as *mut T}
     }
 
     pub fn put(&mut self, v: T) {
-        self.buf.insert(self.inp] = v;
-        self.inp = (self.inp + 1) % self.buf.capacity();
+        unsafe {
+            let p = self.buf.offset(self.inp as int);
+            ptr::write(&mut *p, v)
+        }
+        self.inp = (self.inp + 1) % self.size;
     }
 
-    pub fn get(&mut self) -> Option<T> {
-        let v = self.buf.remove(self.outp);
-        self.outp = (self.outp + 1) % self.buf.capacity();
-        v
+    pub fn get(&mut self) -> T {
+        let r: T;
+        unsafe {
+            let p = self.buf.offset(self.outp as int);
+            r = ptr::read(p as *const T);
+        }
+        self.outp = (self.outp + 1) % self.size;
+        r
+    }
+
+    pub fn size(&self) -> uint {
+        (self.inp - self.outp) % self.size
     }
 }
 
 #[cfg(test)]
 mod test {
+
+    use quickcheck::quickcheck;
     use super::Queue;
+
     #[test]
     fn queue_size() {
-        assert_eq!(10, Queue::<String>::new(10).size());
+        assert_eq!(0, Queue::<String>::new(10).size());
+    }
+
+    #[test]
+    #[should_fail]
+    fn size0() {
+        Queue::<uint>::new(0);
     }
 
     #[test]
@@ -46,6 +75,15 @@ mod test {
         let mut q = Queue::new(10u);
         q.put(1);
         q.put(2);
-        assert_eq!(Some(1u), q.get());
+        assert_eq!(1u, q.get());
+        assert_eq!(2u, q.get());
+    }
+
+    #[test]
+    fn qc_prop_zero_size() {
+        fn prop_zero_size(sz: uint) -> bool {
+            Queue::<uint>::new(sz).size() == 0
+        }
+        quickcheck(prop_zero_size);
     }
 }
